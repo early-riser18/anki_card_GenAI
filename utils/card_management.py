42a5041c2f798c8ee9220c0c5b69df8da03fa90a
Justin <justin.warambourg@gmail.com>
2023-09-08 18:21:11 +0800

    implement interface for flashcard services

--- utils/card_management.py
+++ utils/card_management.py
@@ -1,14 +1,35 @@
-#Card manager
-#Flashcard app service
-
-import anki
-import aqt
+# Card manager
+# Flashcard app service
+import pdb
 
 
 
+class FlashCardServiceInterface:
+    
 
-class FlashCardService():
-    pass
+    def connect_to_service(self) -> bool:
+        """
+        Checks that service is available at initialization
+        """
+        pass
+    def getDeckLists() -> list[str]:
+        "Return a list of all the decks of cards available"
+        pass
 
-class CardManager():
-    pass
+
+
+class CardManager:
+    def __init__(
+        self, flashCardService: FlashCardServiceInterface, sentenceGenerator: str, lang: str
+    ) -> None:
+     
+        self.lang = lang
+        self.sentenceGenerator = sentenceGenerator
+        self.flashCardService = flashCardService
+       
+    
+
+
+
+if __name__ == "__main__":
+    CardManager("anki","en",)
